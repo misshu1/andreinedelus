84574117b4770deaf7d68049a52391a1b261850d
misshu1 <nedelusandrei@gmail.com>
2025-02-10 00:03:54 +0200

    updated urls

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,22 @@
 export const EXTERNAL_URL = {
-	GITHUB: "https://github.com/misshu1",
-	LINKEDIN: "https://www.linkedin.com/in/andreinedelus",
-	INSTAGRAM: "https://www.instagram.com/andreinedelus",
-	ELEVEN_WINDOWS: "https://elevenwindows.dev",
+	GITHUB: {
+		URL: "https://github.com/misshu1",
+		TITLE: "misshu1",
+		ARIA_LABEL: "Github",
+	},
+	LINKEDIN: {
+		URL: "https://www.linkedin.com/in/andreinedelus",
+		TITLE: "andreinedelus",
+		ARIA_LABEL: "Linkedin",
+	},
+	INSTAGRAM: {
+		URL: "https://www.instagram.com/andreinedelus",
+		TITLE: "andreinedelus",
+		ARIA_LABEL: "Instagram",
+	},
+	ELEVEN_WINDOWS: {
+		URL: "https://elevenwindows.dev",
+		TITLE: "elevenwindows.dev",
+		ARIA_LABEL: "Eleven Windows",
+	},
 };
