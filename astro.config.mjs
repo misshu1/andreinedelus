2b1acff6a8ee05c33d29fe3334ca44403db028c5
misshu1 <nedelusandrei@gmail.com>
2026-08-16 21:15:31 +0300

    added year on projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,27 +8,33 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://andreinedelus.com',
-  integrations: [
-    react({include: ['**/react/*']}),
-    icon({
-        include: {
-            ri: ['github-fill', 'linkedin-box-fill', 'alert-line','checkbox-circle-line', 'external-link-line', 'home-4-fill', 'instagram-line'],
-        },
-        iconDir: "src/assets/icons",
-    }),
-    sitemap()
-  ],
-  adapter: netlify(),
-  vite: {
-      build :{
-          sourcemap: true,
-      }
-  },
-  env: {
-      schema: {
-          INSTAGRAM_QUERY_HASH: envField.string({ context: "server", access: "secret" }),
-          INSTAGRAM_ID: envField.number({ context: "server", access: "secret" }),
-      }
+    site:'https://andreinedelus.com',
+    integrations: [
+        react({include: ['**/react/*']}),
+        icon({
+            include: {
+                ri: ['github-fill', 'linkedin-box-fill', 'alert-line','checkbox-circle-line', 'external-link-line', 'home-4-fill', 'instagram-line'],
+            },
+            iconDir: "src/assets/icons",
+        }),
+        sitemap()
+    ],
+    adapter: netlify(),
+    vite: {
+        build :{
+            sourcemap: true,
+            cssCodeSplit: true,
+            rollupOptions: {
+                output: {
+                    experimentalMinChunkSize: 8000,
+                }
+            }
+        }
+    },
+    env: {
+        schema: {
+            INSTAGRAM_QUERY_HASH: envField.string({ context: "server", access: "secret" }),
+            INSTAGRAM_ID: envField.number({ context: "server", access: "secret" }),
+        }
     },
 });
